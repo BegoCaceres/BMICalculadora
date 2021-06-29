5b95c73e57047679be56ee76c033d2699dc29797
Bego <bego.caceres@gmail.com>
2021-06-29 12:43:29 +0200

    Sexo Dinamico

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -7,38 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
   //variables por defecto
-  edad= 25;
+  edad = 25;
   peso = 60;
+  altura = 170;
+  sexo = "Masculino";
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  
+
   //funcion que resta a la edad o al peso segun se mande e o p
-  Menos(s:string):void{
-   if (s === 'p')
-   {
-     this.peso = this.peso - 1;
-   }
-   else
-   if (s === 'e')
-   {
-    this.edad = this.edad - 1;
-   }
+  Menos(s: string): void {
+    if (s === 'p') {
+      this.peso = this.peso - 1;
+    }
+    else
+      if (s === 'e') {
+        this.edad = this.edad - 1;
+      }
   }
 
   //funcion que suma a la edad o al peso segun se mande e o p
-  Mas(s:string):void{
-    if (s === 'p')
-    {
+  Mas(s: string): void {
+    if (s === 'p') {
       this.peso = this.peso + 1;
     }
     else
-    if (s === 'e')
-    {
-     this.edad = this.edad + 1;
-    }
-   }
+      if (s === 'e') {
+        this.edad = this.edad + 1;
+      }
+  }
+  
+  //capturamos el value del evento para saber el valor del boton
+  cambiarAltura(event: any) {
+    console.log(event.target.value);
+    this.altura = event.target.value;
+  }
+
+  //
+  femenino(){
+    this.sexo = "Femenino";
+  }
+
+  masculino(){
+    this.sexo = "Masculino";
+  }
 
 }
