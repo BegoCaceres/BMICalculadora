70f4a06b71296149be2910f47240724b8ab108a4
Bego <bego.caceres@gmail.com>
2021-06-29 12:10:04 +0200

    Peso Edad Dinamico

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+  //variables por defecto
+  edad= 25;
+  peso = 60;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  //funcion que resta a la edad o al peso segun se mande e o p
+  Menos(s:string):void{
+   if (s === 'p')
+   {
+     this.peso = this.peso - 1;
+   }
+   else
+   if (s === 'e')
+   {
+    this.edad = this.edad - 1;
+   }
+  }
+
+  //funcion que suma a la edad o al peso segun se mande e o p
+  Mas(s:string):void{
+    if (s === 'p')
+    {
+      this.peso = this.peso + 1;
+    }
+    else
+    if (s === 'e')
+    {
+     this.edad = this.edad + 1;
+    }
+   }
 
 }
