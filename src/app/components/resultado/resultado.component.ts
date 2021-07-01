88f2e7acff78a8d6e760db81a6d3ea85237d869b
Bego <bego.caceres@gmail.com>
2021-07-01 11:28:25 +0200

    Calculadora BMI v1

--- src/app/components/resultado/resultado.component.ts
+++ src/app/components/resultado/resultado.component.ts
@@ -9,14 +9,33 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ResultadoComponent implements OnInit {
   bmi:number;
+  resultado:string;
+  interpretacion:string;
 
   //inyectamos para poder atrapar la variable valor = BMI
   constructor(private route:ActivatedRoute) { 
+    this.resultado = "";
+    this.interpretacion = "";
     //el + es para parsear el valor string a number y el ! porque el objeto puede ser null para que no de error
     this.bmi = +route.snapshot.paramMap.get('valor')!;
   }
-
+   
+  //llamara a la funcion obtener resultado justo al cargar el componente
   ngOnInit(): void {
+    this.getResultado();
+  }
+
+  getResultado(){
+   if (this.bmi >= 25){
+     this.resultado = "Exceso de peso";
+     this.interpretacion = "Tienes un peso corporal superior al normal. ¡Intente hacer más ejercicio!"
+   } else if (this.bmi >= 18.5){
+     this.resultado = "Normal";
+     this.interpretacion ="Tienes un peso corporal normal. ¡Buen Trabajo!";
+   } else {
+     this.resultado = "Bajo peso";
+     this.interpretacion = "Tienes un peso corporal más bajo de lo normal. ¡Puedes comer un poco más!"
+   }
   }
 
 }
